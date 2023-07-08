b11687c865821a4ecdd5b2c40b608fa9c79e083e
Dash10107 <dakshcjain@gmail.com>
2023-07-08 15:19:12 +0530

    Completed Project

--- app/index.js
+++ app/index.js
@@ -1,31 +1,5 @@
-import { useState } from "react";
-import { View, ScrollView, SafeAreaView } from "react-native";
-import { Stack, useRouter } from "expo-router";
-import { COLORS, icons, images, SIZES } from "../constants";
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
+import { Redirect } from "expo-router";
 
-const Home = () => {
-  const router = useRouter();
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerLeft: () => <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />,
-          headerRight: () => <ScreenHeaderBtn iconUrl={images.profile} dimensions="100%" />,
-          headerTitle: "",
-        }}
-      />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={{ flex: 1, backgroundColor: COLORS.lightWhite, padding: SIZES.medium }}>
-          <Welcome />
-          <Popularjobs />
-          <Nearbyjobs />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-export default Home;
+export default function Index() {
+    return <Redirect href="/home" />;
+}
--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { TouchableOpacity,Image } from 'react-native'
-import styles from './screenheader.style'
+import { Image, TouchableOpacity } from "react-native";
 
-const ScreenHeaderBtn = ({iconUrl,dimensions,handlePress}) => {
+import styles from "./screenheader.style";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
   return (
     <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
       <Image
-      source={iconUrl}
-      resizeMode='cover'
-      style={styles.btnImg(dimensions)}
-       />
+        source={iconUrl}
+        resizeMode='cover'
+        style={styles.btnImg(dimension)}
+      />
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default ScreenHeaderBtn
+export default ScreenHeaderBtn;
